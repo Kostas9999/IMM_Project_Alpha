6717692634edf95b38e86029595e9d3b9be53005
KostuX <goldiskiker@gmail.com>
2021-11-25 22:45:53 +0000

    Sub1

---
+++ Drone/Assets/Scripts/GameMngr.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class GameMngr : MonoBehaviour
+{
+
+    //public Button gitHub_Button;
+    public GameObject game_Over;
+    public static GameObject playZone;
+    public static bool alive;
+    public int score, time, health;
+    public static int TopScore = 299;
+    public bool timeToPlay = false;
+
+    public bool survived = false;
+
+    float current_Time;
+    float countDown_Time = 4;
+    public TextMeshProUGUI countDown_Text;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playZone = GameObject.Find("PlayZone");
+
+        current_Time = (int)countDown_Time;
+
+        alive = true;
+        score = 0;
+        time = 0;
+        health = 100;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alive && timeToPlay) { time = (((int)playZone.transform.localScale.x / 10) - 3); }
+        if(survived){ game_Over.gameObject.SetActive(true);}
+       
+        isTimeTo_Play();
+    }
+
+
+
+    public void deduct_Health()
+    {
+        if(!SceneManager.GetActiveScene().name.Equals("FreeRun") ) // no damage in practice
+        {
+        if (health > 5 ) { health -= 5; }
+        else
+        {
+            health = 0;
+            gameOver();
+            alive = false;
+        }
+        }
+    }
+
+    public void gameOver() { game_Over.gameObject.SetActive(true); }
+
+
+ 
+
+    public void MainMenu_Scene() { SceneManager.LoadScene(0); }
+    public void cube_Scene() { SceneManager.LoadScene(1); }
+    public void freeRun_Scene() { SceneManager.LoadScene(2); }
+     public void restart_Game() { Time.timeScale = 1; SceneManager.LoadScene(SceneManager.GetActiveScene().name); }
+
+
+    public void isTimeTo_Play()
+    {
+        if (current_Time < -1) { countDown_Text.text = ""; }          // destry obj instead
+        if (current_Time < 1 && current_Time > -3) { countDown_Text.text = "GO!!!"; }
+        if (current_Time < 4 && current_Time > 0) { countDown_Text.text = current_Time.ToString("0"); }
+        if (current_Time > 3) { countDown_Text.text = "Ready?!"; }
+
+        current_Time -= 1 * Time.deltaTime;
+        timeToPlay = current_Time < 0 ? true : false;
+
+    }
+
+}
---
+++ Drone/Assets/Scripts/Menu.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject Cube_Description;
+    public GameObject Drone_Model;
+    public TextMeshProUGUI Cube_Top_Score;
+    public GameObject FreeRun_Description;
+
+
+
+    string scene = "MainMenu";
+
+
+
+    public void start()
+    {
+        GameMngr.alive = true;
+        SceneManager.LoadScene(scene);
+
+    }
+    public static void start(string scene_temp) { Time.timeScale = 1f; SceneManager.LoadScene(scene_temp); }
+
+    public void cube_Scene()
+    {
+        scene = "Cube";
+        Drone_Model.gameObject.SetActive(false);
+         FreeRun_Description.gameObject.SetActive(false);
+        Cube_Description.gameObject.SetActive(true);
+       
+
+        Cube_Top_Score.text = "Top score is: " + GameMngr.TopScore;
+
+
+
+    }
+
+
+    public void race_Scene()
+    {
+        scene = "MainMenu";
+        //  scene = "Race";
+         FreeRun_Description.gameObject.SetActive(false);
+        Cube_Description.gameObject.SetActive(false);
+        Drone_Model.gameObject.SetActive(true);
+    }
+
+    public void freeRun_Scene()
+    {
+        // scene = "MainMenu";
+        scene = "FreeRun";
+        Drone_Model.gameObject.SetActive(false);
+        Cube_Description.gameObject.SetActive(false);
+        FreeRun_Description.gameObject.SetActive(true);
+    }
+    public void link_GitHub() { Application.OpenURL("https://github.com/Kostas9999/IMM_Project_Alpha"); }
+    public void link_WebGL() { Application.OpenURL("https://kostas9999.github.io/IMM_Project_Alpha/"); }
+
+}
+
+
+
+
